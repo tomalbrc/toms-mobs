886425f4212b5ca6590980983ad847d6c5bcf074
tomalbrc <tomalbrc@me.com>
2024-04-14 20:09:03 +0200

    Force polymer to autohost resourcepack, 1.2.1

--- src/main/java/de/tomalbrc/toms_mobs/TomsMobs.java
+++ src/main/java/de/tomalbrc/toms_mobs/TomsMobs.java
@@ -4,12 +4,19 @@ import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 import de.tomalbrc.toms_mobs.registries.MobRegistry;
 import de.tomalbrc.toms_mobs.registries.SoundRegistry;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class TomsMobs implements ModInitializer {
     public static final String MODID = "toms_mobs";
 
     @Override
     public void onInitialize() {
+        forceAutohost();
+
         PolymerResourcePackUtils.addModAssets(MODID);
         PolymerResourcePackUtils.markAsRequired();
 
@@ -17,4 +24,13 @@ public class TomsMobs implements ModInitializer {
         MobRegistry.registerMobs();
     }
 
+    void forceAutohost() {
+        try {
+            Path path = FabricLoader.getInstance().getGameDir().resolve("polymer/.force_autohost");
+            Files.createDirectories(path.getParent()); // Create parent directories if they don't exist
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
