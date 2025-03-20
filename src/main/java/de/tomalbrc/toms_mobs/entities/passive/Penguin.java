00a043e1fe1ce140f4b42fe6fb4e5b23ceb5ef55
tomalbrc <tomalbrc@me.com>
2025-03-20 10:19:23 +0100

    cleanup

--- src/main/java/de/tomalbrc/toms_mobs/entities/passive/Penguin.java
+++ src/main/java/de/tomalbrc/toms_mobs/entities/passive/Penguin.java
@@ -75,7 +75,7 @@ public class Penguin extends Animal implements AnimatedEntity, RangedAttackMob {
         this.moveControl = new SemiAquaticMoveControl(this);
         this.jumpControl = new JumpControl(this);
 
-        this.holder = new LivingEntityHolder(this, MODEL);
+        this.holder = new LivingEntityHolder<>(this, MODEL);
         EntityAttachment.ofTicking(this.holder, this);
     }
 
