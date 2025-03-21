a9488828c1b252e413e5e2ff902fdbe6bde19480
tomalbrc <tomalbrc@me.com>
2025-03-21 12:25:57 +0100

    fix: faster tuna

--- src/main/java/de/tomalbrc/toms_mobs/entity/passive/Tuna.java
+++ src/main/java/de/tomalbrc/toms_mobs/entity/passive/Tuna.java
@@ -31,7 +31,7 @@ public class Tuna extends AbstractFish implements AnimatedEntity {
     @NotNull
     public static AttributeSupplier.Builder createAttributes() {
         return AbstractFish.createAttributes()
-                .add(Attributes.MOVEMENT_SPEED, 0.6)
+                .add(Attributes.MOVEMENT_SPEED, 0.7)
                 .add(Attributes.MAX_HEALTH, 16.0);
     }
 
