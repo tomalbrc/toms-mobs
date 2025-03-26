3d4f9686ea48438c0072353cd780959ea6d26bfe
tomalbrc <tomalbrc@me.com>
2025-03-26 11:09:57 +0100

    fix: elephant entity scale

--- src/main/java/de/tomalbrc/toms_mobs/util/LivingEntityHolder.java
+++ src/main/java/de/tomalbrc/toms_mobs/util/LivingEntityHolder.java
@@ -64,13 +64,6 @@ public class LivingEntityHolder<T extends LivingEntity & AnimatedEntity> extends
         }
         matrix4f.scaleLocal(this.entityScale);
 
-        if (this.entityScale != 1.0F) {
-            matrix4f.scale(pose.readOnlyScale());
-        } else {
-            matrix4f.scale(pose.readOnlyScale());
-        }
-
-
         display.element().setTransformation(matrix4f);
         display.element().startInterpolationIfDirty();
     }
