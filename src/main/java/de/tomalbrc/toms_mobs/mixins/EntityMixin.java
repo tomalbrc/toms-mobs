57381530a4a5ad3e6c95726fd31367669723a90c
tomalbrc <tomalbrc@me.com>
2025-04-24 00:20:00 +0200

    fix: elephant controls

---
+++ src/main/java/de/tomalbrc/toms_mobs/mixins/EntityMixin.java
@@ -0,0 +1,21 @@
+package de.tomalbrc.toms_mobs.mixins;
+
+import de.tomalbrc.bil.api.AnimatedEntity;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Shadow public abstract boolean hasControllingPassenger();
+
+    @Inject(method = "isLocalInstanceAuthoritative", at = @At(value = "RETURN"), cancellable = true)
+    public void tomsmobs$isLocalInstanceAuthoritative(CallbackInfoReturnable<Boolean> cir) {
+        if (this instanceof AnimatedEntity && this.hasControllingPassenger()) {
+            cir.setReturnValue(true);
+        }
+    }
+}
