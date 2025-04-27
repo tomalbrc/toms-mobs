b86027e8455751c67bb157026d74bf35b9c8b2ad
tomalbrc <tomalbrc@me.com>
2025-04-27 23:38:20 +0200

    fix: use existing mob ids for dfu

--- src/main/java/de/tomalbrc/toms_mobs/mixins/V1460Mixin.java
+++ src/main/java/de/tomalbrc/toms_mobs/mixins/V1460Mixin.java
@@ -2,11 +2,11 @@ package de.tomalbrc.toms_mobs.mixins;
 
 import com.mojang.datafixers.schemas.Schema;
 import com.mojang.datafixers.types.templates.TypeTemplate;
-import de.tomalbrc.toms_mobs.util.Util;
+import de.tomalbrc.toms_mobs.entity.hostile.*;
+import de.tomalbrc.toms_mobs.entity.passive.*;
 import net.minecraft.util.datafix.schemas.V1460;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -25,29 +25,24 @@ public abstract class V1460Mixin {
         method = "registerEntities",
         at = @At("RETURN")
     )
-    public void registerEntities(Schema schema, CallbackInfoReturnable<Map<String, Supplier<TypeTemplate>>> cir) {
+    public void tm$registerEntities(Schema schema, CallbackInfoReturnable<Map<String, Supplier<TypeTemplate>>> cir) {
         var map = cir.getReturnValue();
-        registerMob(schema, map, mod("ice_cluster"));
-        registerMob(schema, map, mod("ice_spike"));
-        registerMob(schema, map, mod("ice_spike_small"));
-        registerMob(schema, map, mod("iceologer"));
-        registerMob(schema, map, mod("sculkling"));
-        registerMob(schema, map, mod("showmaster"));
-        registerMob(schema, map, mod("snake"));
-        registerMob(schema, map, mod("butterfly"));
-        registerMob(schema, map, mod("capybara"));
-        registerMob(schema, map, mod("elephant"));
-        registerMob(schema, map, mod("firemoth"));
-        registerMob(schema, map, mod("lobster"));
-        registerMob(schema, map, mod("mantaray"));
-        registerMob(schema, map, mod("nautilus"));
-        registerMob(schema, map, mod("penguin"));
-        registerMob(schema, map, mod("seagull"));
-        registerMob(schema, map, mod("tuna"));
-    }
-
-    @Unique
-    private static String mod(String path) {
-        return Util.id(path).toString();
+        registerMob(schema, map, IceCluster.ID.toString());
+        registerMob(schema, map, IceSpike.ID.toString());
+        registerMob(schema, map, IceSpikeSmall.ID.toString());
+        registerMob(schema, map, Iceologer.ID.toString());
+        registerMob(schema, map, Sculkling.ID.toString());
+        registerMob(schema, map, Showmaster.ID.toString());
+        registerMob(schema, map, Snake.ID.toString());
+        registerMob(schema, map, Butterfly.ID.toString());
+        registerMob(schema, map, Capybara.ID.toString());
+        registerMob(schema, map, Elephant.ID.toString());
+        registerMob(schema, map, Firemoth.ID.toString());
+        registerMob(schema, map, Lobster.ID.toString());
+        registerMob(schema, map, Mantaray.ID.toString());
+        registerMob(schema, map, Nautilus.ID.toString());
+        registerMob(schema, map, Penguin.ID.toString());
+        registerMob(schema, map, Seagull.ID.toString());
+        registerMob(schema, map, Tuna.ID.toString());
     }
 }
