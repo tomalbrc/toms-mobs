b82710f59612a8ad08056cc5704c37cabfd33a4c
tomalbrc <tomalbrc@me.com>
2025-05-24 02:28:46 +0200

    Revert "fix: elephant head rotation"
    This reverts commit ad34a72d5f9fbcdc56501589a2f56973f740262c.

--- src/main/java/de/tomalbrc/toms_mobs/util/LivingEntityHolder.java
+++ src/main/java/de/tomalbrc/toms_mobs/util/LivingEntityHolder.java
@@ -35,6 +35,7 @@ public class LivingEntityHolder<T extends LivingEntity & AnimatedEntity> extends
         Matrix4f matrix4f = new Matrix4f();
         matrix4f.translate(pose.readOnlyTranslation().sub(0f, parent.getBbHeight()/this.entityScale, 0f, new Vector3f()));
         matrix4f.rotate(pose.readOnlyLeftRotation());
+        matrix4f.scale(new Vector3f(1.f));
         matrix4f.rotate(pose.readOnlyRightRotation());
 
         var node = getRotationParent(display.node());
@@ -46,9 +47,12 @@ public class LivingEntityHolder<T extends LivingEntity & AnimatedEntity> extends
             var y = (Mth.DEG_TO_RAD * Mth.rotLerp(0.5F, -this.parent.yHeadRotO + this.parent.yBodyRotO, -this.parent.yHeadRot + this.parent.yBodyRot));
             var x = (Mth.DEG_TO_RAD * Mth.lerp(0.5F, this.parent.xRotO, this.parent.getXRot()));
 
-            Vector3f pivotOffset = node.transform().globalTransform().origin(new Vector3f());
+            Vector3f pivotOffset = node.transform().origin().get(new Vector3f()).mul(1, 0, 1);
             matrix4f.translateLocal(pivotOffset);
-            matrix4f = new Matrix4f().rotateY(y).rotateX(x).mul(matrix4f);
+
+            matrix4f.rotateLocalX(x);
+            matrix4f.rotateLocalY(y);
+
             matrix4f.translateLocal(pivotOffset.negate());
         }
 
@@ -57,10 +61,14 @@ public class LivingEntityHolder<T extends LivingEntity & AnimatedEntity> extends
             matrix4f.rotateLocalZ(-this.deathAngle * ((float)Math.PI / 2F));
             matrix4f.translateLocal(0, -this.parent.getBbHeight(), 0);
         }
-
         matrix4f.scaleLocal(this.entityScale);
 
-        matrix4f.scale(pose.readOnlyScale());
+        if (this.entityScale != 1.0F) {
+            matrix4f.scale(pose.readOnlyScale());
+        } else {
+            matrix4f.scale(pose.readOnlyScale());
+        }
+
 
         display.element().setTransformation(matrix4f);
         display.element().startInterpolationIfDirty();
